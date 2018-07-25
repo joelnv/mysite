6747f2bcfe9cd4856cf56ca00b8edfad8ddf0ccc
sudhathankavel <sudhathankavel88@gmail.com>
2018-07-25 16:26:39 +0530

    Jst trail

--- polls/views.py
+++ polls/views.py
@@ -30,25 +30,63 @@
 #     return HttpResponse("You're voting on question %s." % question_id)
 
 from django.shortcuts import render,get_object_or_404
-# from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 # from django.template import loader
 # from django.http import Http404
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    """commented , since this part is needed only if we are not using template"""
-    """starts"""
-    # output = ",".join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    """Ends"""
-    # template = loader.get_template("polls/index.html")
-    context ={'latest_question_list': latest_question_list}
-    return render(request, "polls/index.html", context)
-    # return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+    # latest_question_list =
+    # """commented , since this part is needed only if we are not using template"""
+    # """starts"""
+    # # output = ",".join([q.question_text for q in latest_question_list])
+    # # return HttpResponse(output)
+    # """Ends"""
+    # # template = loader.get_template("polls/index.html")
+    # context ={'latest_question_list': latest_question_list}
+    # return render(request, "polls/index.html", context)
+    # # return HttpResponse(template.render(context, request))
 
 
-def detail(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
-        return render(request, "polls/details.html", {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+# def detail(request, question_id):
+#         question = get_object_or_404(Question, pk=question_id)
+#         return render(request, "polls/details.html", {'question': question})
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/details.html", {'question': question , 'error_message':"You didnt select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+
+
+# def results(request,question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {'question': question})
+
+
